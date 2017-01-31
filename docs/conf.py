b31c9d811bc9d532571c7d131ed9ed02d02196ad
Tibor Simko <tibor.simko@cern.ch>
2017-01-31 18:25:46 +0100

    docs: addition of Travis/Coveralls/RFTD/Waffle/Travis/License badges
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- docs/conf.py
+++ docs/conf.py
@@ -26,12 +26,14 @@ from __future__ import print_function
 import os
 import sphinx.environment
 
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
 # needs_sphinx = '1.0'
+
+# Do not warn on external images.
+suppress_warnings = ['image.nonlocal_uri']
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
