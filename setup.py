fd77426fe9409659153f5786ae3fbc2ecc3d66ee
Marco Vidal García <marco.vidal@cern.ch>
2021-10-08 13:39:18 +0200

    installation: bump shared modules

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ readme = open("README.rst").read()
 history = open("CHANGES.rst").read()
 
 tests_require = [
-    "pytest-reana>=0.8.0a6,<0.9.0",
+    "pytest-reana>=0.8.0a7,<0.9.0",
 ]
 
 extras_require = {
@@ -54,8 +54,8 @@ install_requires = [
     "Werkzeug>=1.0.1,<2.0",
     "fs>=2.0",
     "marshmallow>2.13.0,<=2.20.1",
-    "reana-commons[kubernetes]>=0.8.0a21,<0.9.0",
-    "reana-db>=0.8.0a20,<0.9.0",
+    "reana-commons[kubernetes]>=0.8.0a31,<0.9.0",
+    "reana-db>=0.8.0a21,<0.9.0",
     "htcondor==8.9.11",
     "retrying>=1.3.3",
 ]
