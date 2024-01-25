8e74a85c90df00c8734a6cdd81597f583d11d566
Tibor Šimko <tibor.simko@cern.ch>
2024-01-25 12:20:34 +0100

    ci(shellcheck): fix exit code propagation (#425)
    Return properly the exit code status of shellcheck command when looping
    through shell script files.

--- etc/job_wrapper.sh
+++ etc/job_wrapper.sh
@@ -7,15 +7,16 @@
 # around by dumping command to a
 # temporary wrapper file.
 tmpjob=$(mktemp -p .)
-chmod +x $tmpjob
+chmod +x "$tmpjob"
 echo "command to execute:"
 # $@ input is base64 encoded string of command to execute
+# shellcheck disable=SC2068,SC2294
 eval $@
 
-echo "$@" "|bash" > $tmpjob
-bash $tmpjob
+echo "$@" "|bash" > "$tmpjob"
+bash "$tmpjob"
 res=$?
-rm $tmpjob
+rm "$tmpjob"
 
 if [ $res != 0 ]; then
     echo "[Error] Execution failed with error code: $res"
