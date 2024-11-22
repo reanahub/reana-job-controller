b975c050d2928ee0b979b7bf5310f4ee0618c140
Alputer <alp.tuna.453@gmail.com>
2024-11-22 14:25:52 +0100

    fix(config): update vomsproxy fallback image (#475)

--- reana_job_controller/config.py
+++ reana_job_controller/config.py
@@ -66,7 +66,7 @@ SUPPORTED_COMPUTE_BACKENDS = os.getenv(
 
 
 VOMSPROXY_CONTAINER_IMAGE = os.getenv(
-    "VOMSPROXY_CONTAINER_IMAGE", "docker.io/reanahub/reana-auth-vomsproxy:1.2.0"
+    "VOMSPROXY_CONTAINER_IMAGE", "docker.io/reanahub/reana-auth-vomsproxy:1.3.0"
 )
 """Default docker image of VOMSPROXY sidecar container."""
 
