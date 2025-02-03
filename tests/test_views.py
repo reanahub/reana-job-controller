4d6db0871285a4fdd7e7a5b795d7fdb6e7101dd6
Tibor Šimko <tibor.simko@cern.ch>
2025-02-03 17:57:16 +0100

    chore(master): bump all required packages as of 2025-02-03 (#480)

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of REANA.
-# Copyright (C) 2018, 2019, 2020, 2021 CERN.
+# Copyright (C) 2018, 2019, 2020, 2021, 2025 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""REST API test for REANA-Job-Controller. """
+"""REST API test for REANA-Job-Controller."""
 
 import json
 import uuid
